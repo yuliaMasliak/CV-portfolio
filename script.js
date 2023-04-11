7ca1c0a9d60596ec4c0c101d940fcf9cd8580ee4
Yulia Masliak <1985kozak@gmail.com>
2023-04-11 13:22:47 +0300

    feat: add separate js file

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-function duplicateEncode(word) {
-  let arr = [];
-  let res = '';
-  let wordRes = word.toLowerCase().split('');
-  wordRes.forEach((el) => {
-    let count = 0;
-    for (let i = 0; i < wordRes.length; i++) {
-      if (el == wordRes[i]) {
-        count += 1;
-      }
-    }
-    count > 1 ? (res += ')') : (res += '(');
-  });
-  return res;
-}
+document.querySelector('#burger-logo').addEventListener('click', () => {
+  document
+    .querySelector('.burger-content')
+    .classList.toggle('burger-content-show');
+});
