e0793223246b776fc0e1d8f95bf9b928ad5ab9fb
Yulia Masliak <1985kozak@gmail.com>
2023-08-07 23:15:09 +0300

    feat: add styles

--- cv/src/app/core/skills/skills.component.ts
+++ cv/src/app/core/skills/skills.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 interface Skill {
   img: string;
   name: string;
